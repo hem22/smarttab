5767d7381aea08ba816013bd7f910374740c6286
hem22 <reabt12@gmail.com>
2019-05-10 13:02:23 +0900

    #1 스위치 오류남

--- src/main/java/com/example/smart3/home1.java
+++ src/main/java/com/example/smart3/home1.java
@@ -10,13 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.Toast;
 import static android.widget.Toast.LENGTH_LONG;
 
 public class home1 extends Fragment {
 
-    Switch switch1, switch2;
+    ImageView alramoff_img,alramoff2_img;
+    private Switch switch1, switch2;
+
     public Object home1;
     public static home1 newInstance(){
         home1 fragment = new home1();
@@ -28,17 +31,50 @@ public class home1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.activity_home1,container,false);
-        switch1 = (Switch) view.findViewById(R.id.switch1);
-        switch2 = (Switch) view.findViewById(R.id.switch2);
         return view;
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+     /*   switch1 = (Switch)findViewById(R.id.switch1);
+        switch2 = (Switch)findViewById(R.id.switch2);
+        alramoff_img = (ImageView)findViewById(R.id.alramoff_img);
+        alramoff2_img = (ImageView)findViewById(R.id.alramoff2_img);
+        */
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true) {
+                    Toast.makeText((Context) home1, "전기가 켜집니다", LENGTH_LONG).show();
+                    alramoff_img.setImageResource(R.drawable.alram_on);
+
+                    // 전기켜지고
+                } else {
+                    Toast.makeText((Context) home1, "전기가 꺼집니다.", LENGTH_LONG).show();
+                    alramoff_img.setImageResource(R.drawable.alram_off);
+
+                    // 꺼지고
+                }
+            }
+        });
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true) {
+                    Toast.makeText((Context) home1, "전기가 켜집니다", LENGTH_LONG).show();
+                    alramoff2_img.setImageResource(R.drawable.alram_on);
+                } else {
+                    Toast.makeText((Context) home1, "전기가 꺼집니다.", LENGTH_LONG).show();
+                    alramoff2_img.setImageResource(R.drawable.alram_off);
+                }
+            }
+        });
 
 
-
+/*
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
                                                @Override
                                                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
@@ -63,6 +99,7 @@ public class home1 extends Fragment {
                                                }
                                            }
         );
+        */
     }
 }
 
